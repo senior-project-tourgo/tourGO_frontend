9ac14e91e3312dd7e1e9b01c15c91dcc625be1d8
Jidapa <shinjidapa@gmail.com>
2026-01-17 22:11:55 +0700

    fix: navigation dependencies and minor config tweaks
    add and fix dependencies and fix some layout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,5 @@
 import { Slot } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import '../global.css';
-
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return <Slot />;
