29bab11e1b78240edca0b9de449b9e4f4d724a9f
Jidapa <shinjidapa@gmail.com>
2026-02-25 20:13:56 +0700

    feat: add close button and adjust PlaceCard layout
    Introduce optional showCross and onPressCross props to PlaceCard

--- components/cards/variants/PlaceCard/PlaceCard.tsx
+++ components/cards/variants/PlaceCard/PlaceCard.tsx
@@ -11,17 +11,25 @@ import { Badge } from '@/components/Badge';
 interface PlaceCardProps extends BaseCardProps {
   place: Place;
   onPress?: (place: Place) => void;
+  showCross?: boolean;
+  onPressCross?: (place: Place) => void;
 }
 
-export function PlaceCard({ place, onPress }: PlaceCardProps) {
+export function PlaceCard({
+  place,
+  onPress,
+  showCross = false,
+  onPressCross
+}: PlaceCardProps) {
   const openingHours = getPlaceOpeningStatus(place.openingHours);
 
   return (
     <Pressable onPress={() => onPress?.(place)}>
-      <View className="h-40 flex-row gap-4 rounded-2xl bg-white p-4 shadow-sm">
+      <View className="h-50 w-100 relative flex-row gap-4 rounded-2xl bg-white p-4 shadow-sm">
         <ImageWithFallback
           primaryImageUrl={place.image}
-          className="aspect-square h-full rounded-xl"
+          className="h-40 w-32 rounded-xl"
+          resizeMode="cover"
         />
 
         <View className="gap-1">
@@ -75,6 +83,18 @@ export function PlaceCard({ place, onPress }: PlaceCardProps) {
             ))}
           </View>
         </View>
+
+        {/* Remove Button */}
+        {showCross && (
+          <Pressable
+            onPress={() => {
+              onPressCross?.(place);
+            }}
+            className="absolute right-3 top-3 z-10 bg-white p-1"
+          >
+            <Ionicons name="close" size={18} color={colors.text.DEFAULT[600]} />
+          </Pressable>
+        )}
       </View>
     </Pressable>
   );
